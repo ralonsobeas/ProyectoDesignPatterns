90b321605c0018cce1365f0953c4df3d012303be
davidxia2002 <davidxia0126@gmail.com>
2022-05-02 18:46:57 +0200

    Empolion

--- src/com/utad/designpatterns/enemigos/pokemon/Empolion.java
+++ src/com/utad/designpatterns/enemigos/pokemon/Empolion.java
@@ -1,5 +1,29 @@
 package com.utad.designpatterns.enemigos.pokemon;
 
-public class Empolion {
+import com.utad.designpatterns.enemigos.Enemigo;
+
+/**
+ * Enemigo Empolion
+ * @author david
+ *
+ */
+public class Empolion extends Enemigo{
+
+	/**
+	 * Ataque de Empolion
+	 */
+	public float ataque() {
+		return this.getFuerza() * this.getEstrategia().getFuerza();
+	}
+	
+	/**
+	 * Defensa de Empolion
+	 */
+	public void defensa() {
+		this.setResistencia(getResistencia()*4);
+	}
+	
+	
+	
 
 }
